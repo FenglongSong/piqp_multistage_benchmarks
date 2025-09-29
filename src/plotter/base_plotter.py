d5c235013e6af97063ac4cbb0ca31baa9a4f0844
Longlongago <40928472+FenglongSong@users.noreply.github.com>
2025-09-29 21:49:28 +0200

    add plot style for multistage parallel solver

--- src/plotter/base_plotter.py
+++ src/plotter/base_plotter.py
@@ -12,6 +12,9 @@ class BasePlotter:
             'piqp_block': '#ff7f0e',   # orange
             'piqp_sse': '#ff7f0e',     # orange
             'piqp_avx2': '#2ca02c',    # green
+            'piqp_block': '#ff7f0e',   # light orange
+            'piqp_sse_p': '#ffb84d',   # light orange
+            'piqp_avx2_p': '#b5e07a',  # light green
             'hpipm': '#d62728',        # red
             'qpalm': '#9467bd',        # purple
             'osqp': '#8c564b',         # brown
@@ -21,6 +24,9 @@ class BasePlotter:
             'piqp_block': 'PIQP (block)',
             'piqp_sse': 'PIQP (block SSE)',
             'piqp_avx2': 'PIQP (block AVX2)',
+            'piqp_block_p': 'PIQP (block par)',
+            'piqp_sse_p': 'PIQP (block SSE par)',
+            'piqp_avx2_p': 'PIQP (block AVS2 par)',
             'hpipm': 'HPIPM',
             'qpalm': 'QPALM',
             'osqp': 'OSQP',
